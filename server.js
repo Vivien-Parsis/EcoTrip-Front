34e4ebc1eb1c4ce3df57b5ed8c5c6c65dea7026a
Vivien-Parsis <vivienparsis.perso@gmail.com>
2024-04-17 14:34:05 +0200

    added page 404

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const host = ("RENDER" in process.env || process.env.ISDOCKER == "true") ? `0.0.
 
 app.use('/', express.static('public/page'))
 app.use('/assets', express.static('public/assets'))
+app.use((req, res, next)=>{
+    return res.status(404).sendFile(__dirname + "/public/error/404.html")
+})
 
 app.listen({ host: host, port: port }, () =>{
     console.log(`This server is listen on http://${host}:${port}`)
