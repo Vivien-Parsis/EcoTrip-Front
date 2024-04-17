f8580d4321a062b55e08b2d059bd38b481295ccd
Vivien-Parsis <vivienparsis.perso@gmail.com>
2024-04-17 17:55:04 +0200

    unable to have endpoint without .html

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT ? process.env.PORT : 4000
 const host = ("RENDER" in process.env || process.env.ISDOCKER == "true") ? `0.0.0.0` : `localhost`
 
-app.use('/', express.static('public/page'))
+app.use('/', express.static('public/page',{extensions:["html","htm"]}))
 app.use('/assets', express.static('public/assets'))
 app.use((req, res, next)=>{
     return res.status(404).sendFile(__dirname + "/public/error/404.html")
